bc16f64a9d3895b718caca4d0e83afc73818879b
Angie Kassab <anasab12@gmail.com>
2018-07-02 21:13:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
-
-def countdown
+def countdown(integer)
+  x = 1
+  while x <= 10 
+    puts "#{x} SECOND(S)!"
+    x += 1
+    if x == 0
+      puts "HAPPY NEW YEAR!"
+    end 
+  end
 end
+
